6e9799d0bd6adac9e3c0b00b1a489a27062c0f33
Mickae Truman <mtruman@nyit.edu>
2019-02-13 02:36:23 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,5 @@
-def greeting(name)
-  puts "Hi! I'm HAL, what's your #{name}?\n. Hello #{name}. It's nice to meet you.\n"
-puts "Welcome, please enter your #{name}:"
+def greeting(name)  
+  puts "Hello #{name}. It's nice to meet you."
 end
-greeting("John")
-String after = before.trim().replaceAll(" +", " ");
+
+
